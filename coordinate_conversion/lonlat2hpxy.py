8544df64469dae6ddc88d1a463b0a3e73d745736
Angel Martinez <angel.martinez@monash.edu>
2026-06-08 18:30:33 +1000

    Update warning

--- coordinate_conversion/lonlat2hpxy.py
+++ coordinate_conversion/lonlat2hpxy.py
@@ -30,6 +30,10 @@ def lonlat2hpxy(map_input, crln, crlt):
     import astropy.units as u
     from astropy.coordinates import SkyCoord
     from sunpy.coordinates import frames
+    import warnings
+
+    # Suppress all SunPy warnings
+    warnings.filterwarnings("ignore", module="sunpy")
 
     # Calculate sun radius in meters
     rsun_obs = map_input.rsun_obs.to(u.rad)
